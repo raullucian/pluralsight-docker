c2cbce7eb7b049b6e88ccd78bcd5fcdd7ac56764
raullucian <38651682+raullucian@users.noreply.github.com>
2018-04-23 12:57:03 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,1 +1,13 @@
---reporter spec
+// Simple async test for HTTP 200 response code using supertest
+'use strict';
+
+var request = require("supertest"),
+    app = require("../app").getApp;
+
+describe('GET /', function(){
+  it('expects HTTP response 200', function(done){
+    request(app)
+     .get('/')
+	 .expect(200, done); 
+  });
+});
